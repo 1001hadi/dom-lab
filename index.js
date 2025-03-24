0f8ba52cef2bb9204c7680b65115819169444342
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-03-24 15:23:50 -0600

    2nd part solved

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 let mainEl = document.querySelector("main");
+let topMenuEl = document.getElementById("top-menu");
 
+//part 1
 mainEl.style.backgroundColor = "var(--main-bg)";
 mainEl.innerHTML = `<h1>DOM Manipulation</h1>`;
 mainEl.classList.add("flex-ctr");
+
+//part 2
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
