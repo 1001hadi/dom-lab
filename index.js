48404232ea274d99557664bf2fda4d22522f9d49
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-03-24 15:20:03 -0600

    part  one solved

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-import "./styles.css";
+let mainEl = document.querySelector("main");
 
-
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = `<h1>DOM Manipulation</h1>`;
+mainEl.classList.add("flex-ctr");
